94719c743b25147d5457fa7374de95c84183180f
MarkoEv <markodevp@gmail.com>
2026-03-17 14:35:05 -0600

    Funcion para inicializar Swiper => Plantilla interactiva sumalación de página

---
+++ src/utils/swiper.js
@@ -0,0 +1,11 @@
+export const initSwiper = () => {
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  });
+};
