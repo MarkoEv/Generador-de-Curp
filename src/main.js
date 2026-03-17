9c038f5a0a79c32bd4360402a22eaf44a82a0062
MarkoEv <markodevp@gmail.com>
2026-03-17 14:55:49 -0600

    funciones para inicializar inyeccion de contenido a body

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import { initSwiper } from "./utils/swiper";
 import { typingEfect } from "./utils/typingEfect";
+import { header } from "./components/header";
+import { swiperContent } from "./components/swipersContent";
+
+function insertHtml() {
+  document.body.insertAdjacentHTML("afterbegin", header());
+  document.body.insertAdjacentHTML("beforeend", swiperContent());
+}
 
 document.addEventListener("DOMContentLoaded", () => {
+  insertHtml();
+
   initSwiper();
   typingEfect();
 });
