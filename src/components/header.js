6f64563bca1c9e94486f2fddaa89c70093c5c7f0
MarkoEv <markodevp@gmail.com>
2026-03-18 23:15:52 -0600

    actualizacion de header => html con iconos, botones, información principal

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,63 @@
-export const header = () => {
-  return `
-      <header class="h-full">
-    <div class="swiper-slide text-center place-content-center  dark:bg-black/80 flex justify-center items-center ">
-      <div class="top-0 absolute right-0 p-5 flex justify-between items-center gap-5">
-        <div class="">
-          <a href="https://github.com/MarkoEv/Generador-de-Curp" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github text-gray-950 dark:text-white border rounded-2xl p-0.5"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" /></svg>          </a>
+      export const header = () => {
+    return `
+    <header class="h-full">
+        <div class="swiper-slide text-center place-content-center  dark:bg-black/80 flex justify-center items-center ">
+            <div class="top-0 absolute right-0 p-5 flex justify-between items-center gap-5">
+                <div class="">
+                    <a href="https://github.com/MarkoEv/Generador-de-Curp" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github text-gray-950 dark:text-white border rounded-2xl p-0.5">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path
+                                d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                        </svg> </a>
+                </div>
+                <label
+                    class="flex cursor-pointer justify-end top-0 items-center gap-4 text-gray-950 dark:text-gray-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <circle cx="12" cy="12" r="5" />
+                        <path
+                            d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+                    </svg>
+                    <input type="checkbox" value="synthwave"
+                        class="toggle theme-controller dark:text-gray-100 text-gray-950" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                    </svg>
+                </label>
+            </div>
+            <div class="top-10 absolute right-0 p-5  justify-between items-center gap-5 hidden" id="divTools">
+                <div class="grid gap-5 justify-center">
+                    <a href="/" title="Reiniciar" class="border rounded-2xl border-white opacity-50 hover:opacity-100 cursor-pointer text-3xl p-0.5">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg>
+                    </a>
+                    <button id="slidePrev" title="Anterior" class="border rounded-2xl border-white opacity-50 hover:opacity-100 cursor-pointer text-3xl p-0.5">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M5 12l6 6" /><path d="M5 12l6 -6" /></svg>
+                </button>
+            </div>
         </div>
-        <label class="flex cursor-pointer justify-end top-0 items-center gap-4 text-gray-950 dark:text-gray-100">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <circle cx="12" cy="12" r="5" />
-            <path
-              d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-          </svg>
-          <input type="checkbox" value="synthwave" class="toggle theme-controller dark:text-gray-100 text-gray-950" />
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-          </svg>
-        </label>
-      </div>
-      <div class="">
-        <h1 class="text-2xl text-gray-950 dark:text-gray-100" id="text"></h1>
-        <span class="cursor text-gray-950 dark:text-gray-100 text-2xl" id="lyrics">|</span>
-      </div>
-<button id="initCurp" class="btn border rounded-2xl dark:bg-white w-60 h-20 dark:text-gray-900 text-3xl">Empezar</button>
+            <ul id="renderCurp" class="flex justify-center text-center p-5 gap-1"></ul>
 
-    </div>
-  </header>
+            <div class="">
+                <h1 class="text-2xl text-gray-950 dark:text-gray-100" id="text">
+                </h1>
+                <span class="text-gray-950 dark:text-gray-100 text-2xl" id="lyrics">
+                </span>
+            </div>
+            <div>
+                <button id="initCurp"
+                    class="btn border rounded-2xl dark:bg-white w-60 h-20 dark:text-gray-900 text-3xl">Empezar</button>
+            </div>
+            <div>
+            </div>
+            <div class="py-6">
+                <span id="instructions" class="text-2xl p-5 text-gray-950 dark:text-white"></span>
+            </div>
+        </div>
+    </header>
+
     `;
-};
+    };
