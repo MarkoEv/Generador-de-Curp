e99b422580aab72874fab89e9d6683086076f7e4
MarkoEv <markodevp@gmail.com>
2026-03-18 23:17:13 -0600

    actualizacion de swipersContent.js => sliders principales de swiper : contiene botones, inputs, pagination

--- src/components/swipersContent.js
+++ src/components/swipersContent.js
@@ -1,55 +1,106 @@
-export const swiperContent = () => {
+  export const swiperContent = () => {
+  const spanCursor = `
+  <span class="cursor text-">|</span>
+  `;
+
+  document
+  .getElementById("instructions")
+  .insertAdjacentHTML("afterend", spanCursor);
+
   return `
   <div class="swiper mySwiper w-full h-[60%]" hidden>
     <div class="swiper-wrapper">
-      <div class="swiper-slide text-center  text-2xl dark:bg-black/80 ">
-        <div class="flex justify-center gap-3 ">
-          <div> <div class="grid p-5 text-gray-950 dark:text-white">Nombres</div>
-            <input type="text" id="primerName" class="text-3xl w-100 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl py-1.5 pr-3 pl-1 text-center"
-              placeholder="Jose Luis">
-          </div>
+      
+    <div class="swiper-slide text-center  text-2xl dark:bg-black/80 ">
+        <div class="grid justify-center gap-3 ">
+          <input type="text" id="primerName"
+            class="text-3xl w-100 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl py-1.5 pr-3 pl-1 text-center"
+            placeholder="Ingresa tu nombre">
+          <button id="nextBtnName"
+            class="btn border rounded-2xl dark:bg-white bg-black text-white w-100 h-20 dark:text-gray-900 text-3xl opacity-50"
+            disabled>Siguiente</button>
         </div>
       </div>
+      
 
       <div class="swiper-slide text-center text-2xl dark:bg-black/80 flex justify-center items-center">
-        <div class="grid ">
-          <div class="grid p-5 text-gray-950 dark:text-white">APELLIDOS</div>
-          <div class="">
-            <input type="text" class="text-3xl w-100 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl py-1.5 pr-3 pl-1 text-center"
+          <div class="grid gap-3 justify-center">
+            <input type="text"
+              class="text-3xl w-100 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl py-1.5 pr-3 pl-1 text-center"
               placeholder="Paterno" id="apellidoP">
-            <input type="text" class="text-3xl w-100 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl py-1.5 pr-3 pl-1 text-center"
+            <input type="text"
+              class="text-3xl w-100 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl py-1.5 pr-3 pl-1 text-center"
               placeholder="Materno" id="apellidoM">
+            <button id="nextBtnApe"
+              class="btn border rounded-2xl dark:bg-white w-100 h-20 dark:text-gray-900 text-3xl opacity-50"
+              disabled>Siguiente</button>
           </div>
-        </div>
       </div>
 
-      <div class="swiper-slide text-center text-2xl dark:bg-black/80 flex justify-center items-center">
-        <div class="text-gray-950 dark:text-white p-5">Fecha de nacimiento</div>
-        <div class="flex justify-center gap-3.5">
-        <input id="days"  placeholder="01" class="text-3xl w-50 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl  text-center">
-        <input id="months" placeholder="12" class="text-3xl w-50 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl  text-center">
-        <input id="years" placeholder="2000" class="text-3xl w-50 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl  text-center">
+    <div class="swiper-slide text-center text-2xl dark:bg-black/80 flex justify-center items-center">
+      <div class="text-gray-950 dark:text-white p-5"></div>
+        <div class="grid gap-5 justify-center">
+          <div>
+            <input id="days" type="number" placeholder="Día" min="1" max="31"
+              class="text-3xl w-40 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl  text-center">
+            <input id="months" type="number" min="1" max="12" placeholder="Mes"
+              class="text-3xl w-40 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl  text-center">
+            <input id="years" type="number" min="1900" max="${new Date().getFullYear()}" placeholder="Año"
+              class="text-3xl w-40 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl  text-center">
+          </div>
+          <div>
+            <button id="nextBtnDate"
+              class="btn border rounded-2xl dark:bg-white w-100 h-20 dark:text-gray-900 text-3xl opacity-50"
+              disabled>Siguiente</button>
+          </div>
+      </div>
+    </div>
+
+      <div class="swiper-slide text-center text-2xl dark:bg-black/80 flex justify-center items-center" id="botones">
+        <div class="grid text-gray-950 dark:text-white p-5">Género</div>
+        <button
+          class="btn w-50 h-25 border rounded-2xl border-white opacity-50 hover:opacity-100 cursor-pointer text-3xl"
+          id="H">
+          Masculino
+        </button>
+        <button
+          class="btn w-50 h-25 border rounded-2xl border-white opacity-50 hover:opacity-100 cursor-pointer text-3xl"
+          id="M">
+          Femenino
+        </button>
+        <div class="p-5">
+          <button id="nextBtnGender"
+            class="btn border rounded-2xl dark:bg-white w-100 h-20 dark:text-gray-900 text-3xl opacity-50"
+            disabled>Siguiente</button>
         </div>
       </div>
 
       <div class="swiper-slide text-center text-2xl dark:bg-black/80 flex justify-center items-center">
         <div class="text-gray-950 dark:text-white p-5"> En que Estado naciste?</div>
-        <select id="estates" class="text-3xl w-60 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl py-1.5 pr-3 pl-1 text-center ">
-          <option class="text-black/50">Selecciona</option>
-        </select>
+        <div class="grid gap-5 justify-center">
+          <select id="estates"
+            class="text-3xl w-100 h-20 bg-gray-900 dark:bg-white/5 border rounded-3xl py-1.5 pr-3 pl-1 text-center">
+          </select>
+          <button id="nextBtnEstate"
+            class="btn border rounded-2xl dark:bg-white w-100 h-20 dark:text-gray-900 text-3xl opacity-50 "
+            disabled>Siguiente</button>
+        </div>
       </div>
 
-      <div class="swiper-slide text-center text-2xl dark:bg-black/80 flex justify-center items-center" id="botones">
-        <div class="grid text-gray-950 dark:text-white p-5">Género</div>
-        <button class="btn w-30 h-30 border rounded-2xl border-white cursor-pointer hover:text-green-500" id="H">
-        <svg id="H" xmlns="http://www.w3.org/2000/svg" width="55" height="55" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-man"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8c1.628 0 3.2 .787 4.707 2.293a1 1 0 0 1 -1.414 1.414c-.848 -.848 -1.662 -1.369 -2.444 -1.587l-.849 5.944v4.936a1 1 0 0 1 -2 0v-4h-2v4a1 1 0 0 1 -2 0v-4.929l-.85 -5.951c-.781 .218 -1.595 .739 -2.443 1.587a1 1 0 1 1 -1.414 -1.414c1.506 -1.506 3.08 -2.293 4.707 -2.293z" /><path d="M12 1a3 3 0 1 1 -3 3l.005 -.176a3 3 0 0 1 2.995 -2.824" /></svg>
-        </button>
-        <button class="btn w-30 h-30 border rounded-2xl border-white cursor-pointer hover:text-pink-500" id="M">
-        <svg id="M" xmlns="http://www.w3.org/2000/svg" width="55" height="55" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-woman"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 8c1.91 0 3.79 .752 5.625 2.219a1 1 0 1 1 -1.25 1.562c-1.019 -.815 -2.016 -1.345 -2.997 -1.6l1.584 5.544a1 1 0 0 1 -.962 1.275h-1v4a1 1 0 0 1 -2 0v-4h-2v4a1 1 0 0 1 -2 0v-4h-1a1 1 0 0 1 -.962 -1.275l1.584 -5.545c-.98 .256 -1.978 .786 -2.997 1.601a1 1 0 1 1 -1.25 -1.562c1.733 -1.386 3.506 -2.133 5.307 -2.212l.335 -.007z" /><path d="M12 1a3 3 0 1 1 -3 3l.005 -.176a3 3 0 0 1 2.995 -2.824" /></svg>        
-        </button>
+      <div class="swiper-slide text-2xl dark:bg-black/80 text-center flex justify-center items-center">
+        <div class="grid text-center justify-center justify-items-center gap-5">
+          <input id="digitoEnd" type="number"
+            class="rounded-2xl items-center text-7xl w-25 h-30 bg-gray-900 dark:bg-white/5 border rounded-5xl py-1.5 pr-3 pl-1 text-center"
+            value="" min="0" max="9" disabled>
+          <button id="numberRand" class="btn border rounded-2xl dark:bg-white w-70 h-20 dark:text-gray-900 text-3xl">
+            Generar número</button>
+          <button id="whatIs" class="cursor-pointer text-white hover:text-white top-0">¿Qué es el dígito final?</button>
+        </div>
       </div>
-    </div>
+
+   </div><!-- cierra swiper-wrapper -->
     <div class="swiper-pagination"></div>
-  </div>
+  </div><!-- cierra swiper mySwiper -->
     `;
-};
+    };
+
