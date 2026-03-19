f06da03634649007ee9659931e18605921de3d10
MarkoEv <markodevp@gmail.com>
2026-03-18 23:02:44 -0600

    modelo inicial para manejar el evento de click en btnInitCurp

---
+++ src/models/initButton.js
@@ -0,0 +1,35 @@
+import { textInstruction } from "../utils/instructions";
+import { renderCurpBoxes } from "../components/renderCurpBoxes";
+import { initSwiper } from "../utils/swiper";
+
+// función para manejar el evento del boton principal (comenzar)
+// obejtivo: al dar click iniciar swiper
+
+export function clickNext() {
+  // iniciar swiper
+  const swiper = initSwiper();
+  // boton empezar
+  const btnInitCurp = document.getElementById("initCurp");
+  // contenido del header
+  const header = document.querySelector("header");
+  // contenido html swiper
+  const swiperHtml = document.querySelector(".mySwiper");
+  swiperHtml.classList.remove("swiper-backface-hidden");
+  textInstruction("Descubre como se construye tu curp");
+  
+  // boton para retroceder slide
+  const divTools = document.getElementById("divTools");
+  
+  
+  // cuando se da click mostrar el swiper=>
+  btnInitCurp.addEventListener("click", () => {
+    header.classList.remove("h-full");
+    header.classList.add("h-[40%]");
+    swiperHtml.removeAttribute("hidden");
+    btnInitCurp.remove();
+    textInstruction("Ingresa tus datos");
+    renderCurpBoxes();
+
+    divTools.classList.remove("hidden");
+  });
+}
